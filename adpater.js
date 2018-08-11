618958999ecc54732a1e63173dd6a5e57202b4a1
王麟 <viowan@gmail.com>
2018-08-11 12:35:04 +0800

    adpater.js file add eslint

--- adpater.js
+++ adpater.js
@@ -1,48 +1,33 @@
 const Promise = require("./promise-aplus");
 
 function resolved(value){
-    return new Promise((resolve) => {
-        resolve(value)
-    });
+	return new Promise((resolve) => {
+		resolve(value);
+	});
 }
 
 function rejected(reason){
-    return new Promise((_resolve, reject) => {
-        reject(reason)
-    });
+	return new Promise((_resolve, reject) => {
+		reject(reason);
+	});
 }
 
 function deferred(){
-    let resolve = null;
-    let reject = null;
-    const promise = new Promise(function(innerResolve, innerReject){
-        resolve = innerResolve;
-        reject = innerReject;
-    })
-    return {
-        promise,
-        resolve,
-        reject,
-    }
+	let resolve = null;
+	let reject = null;
+	const promise = new Promise(function(innerResolve, innerReject){
+		resolve = innerResolve;
+		reject = innerReject;
+	});
+	return {
+		promise,
+		resolve,
+		reject,
+	};
 }
 
 module.exports = {
-    deferred,
-    resolved,
-    rejected,
-}
-
-
-function deferred(){
-    let resolve = null;
-    let reject = null;
-    const promise = new Promise(function(innerResolve, innerReject){
-        resolve = innerResolve;
-        reject = innerReject;
-    })
-    return {
-        promise,
-        resolve,
-        reject,
-    }
-}
+	deferred,
+	resolved,
+	rejected,
+};
